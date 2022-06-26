0f4082087b52d74ad736aeb4ea32e9b66dc26c51
Jay Arredondo <jearredondo13@gmail.com>
2022-06-26 05:19:58 -0500

    finished notes components ui

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note"></textarea>
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success ">Add New Note</button>
+        </div>
+      </div>
+    </div>
+
     <div class="card mb-4" v-for="i in 3">
       <div class="card-content">
         <div class="content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe reprehenderit impedit fugiat provident id soluta consequatur deserunt quam libero.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe
+          reprehenderit impedit fugiat provident id soluta consequatur deserunt
+          quam libero.
         </div>
       </div>
       <footer class="card-footer">

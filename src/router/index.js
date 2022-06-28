14554628bdccd8449494bac890fe28b7be73e9cc
Jay Arredondo <jearredondo13@gmail.com>
2022-06-27 19:30:54 -0500

    almost done with auth page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
+import ViewAuth from '../views/ViewAuth.vue';
 import ViewNotes from "../views/ViewNotes.vue";
 import ViewStats from "../views/ViewStats.vue";
-import ViewEditNotes from '../views/ViewEditNotes.vue'
+import ViewEditNotes from '../views/ViewEditNotes.vue';
 
 const routes = [
   {
@@ -20,6 +21,11 @@ const routes = [
     name: "edit-note",
     component: ViewEditNotes,
   },
+  {
+    path: "/auth",
+    name: "auth",
+    component: ViewAuth
+  }
 ];
 
 const router = createRouter({
---
+++ src/views/ViewAuth.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { ref, computed, reactive } from "vue";
+const register = ref(false);
+const formTitle = computed(() => {
+  return register.value === true ? "Register" : "Sign-In";
+});
+
+const credentials = reactive({
+  email: "",
+  password: "",
+});
+
+const onSubmit = () => {
+  if (!credentials.email || !credentials.password) {
+    alert("Please enter an email and password.");
+  } else {
+    if (register.value) {
+      console.log("Register user with these credentials: ", credentials);
+    } else {
+      console.log("Login user with these credentials: ", credentials);
+    }
+  }
+};
+</script>
+<template>
+  <div class="auth">
+    <div class="tabs is-centered">
+      <ul>
+        <li :class="{ 'is-active': !register }">
+          <a @click.prevent="register = false">Sign-In</a>
+        </li>
+        <li :class="{ 'is-active': register }">
+          <a @click.prevent="register = true">Register</a>
+        </li>
+      </ul>
+    </div>
+    <div class="card auth-form">
+      <div class="card-content">
+        <div class="title has-text-centered">
+          {{ formTitle }}
+        </div>
+        <form @submite.prevent="onSubmit">
+          <div class="field">
+            <label class="label">Email: </label>
+            <div class="control">
+              <input
+                v-model="credentials.email"
+                class="input"
+                type="email"
+                placeholder="e.g. alexsmith@gmail.com"
+              />
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Password:</label>
+            <div class="control">
+              <input
+                v-model="credentials.password"
+                class="input"
+                type="password"
+              />
+            </div>
+          </div>
+          <div class="field is-grouped is-grouped-right">
+            <p class="control">
+              <button class="button is-primary">{{ formTitle }}</button>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.auth-form {
+  max-width: 400px !important;
+  margin: 0 auto;
+}
+</style>

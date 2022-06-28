a1e1af42646f8a1b6dac3aeed494c3bd5a2e7186
Jay Arredondo <jearredondo13@gmail.com>
2022-06-28 12:33:17 -0500

    finished setting up navigation guards for router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import ViewAuth from '../views/ViewAuth.vue';
 import ViewNotes from "../views/ViewNotes.vue";
 import ViewStats from "../views/ViewStats.vue";
 import ViewEditNotes from '../views/ViewEditNotes.vue';
-
+import {useAuthStore} from '../stores/authStore.js';
 const routes = [
   {
     path: "/",
@@ -32,5 +32,18 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
+
+// Navigation Guards
+router.beforeEach(async (to, from) => {
+  const authStore = useAuthStore();
+  if (!authStore.user.id && to.name !== 'auth') {
+    return { name: 'auth' }
+  }
+  if(authStore.user.id && to.name === 'auth') {
+    // to prevent the user from leaving the page they're on.
+    // in our case, we prevent users from visiting the auth page from the current page they're on.
+    return false;
+  }
+})
 
 export default router;
